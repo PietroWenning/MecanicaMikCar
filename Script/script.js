d3332af17c305b0e4f18f9982fa6d340fac5e6b6
Pietro calebe <pietrorepositorio@gmail.com>
2025-07-06 02:36:44 -0300

    terminando parte servicos

--- Script/script.js
+++ Script/script.js
@@ -11,3 +11,15 @@ function nextImage(){
     }
 document.getElementById("radio"+count).checked = true;
 }
+function Mostrarparagrafo(botao) {
+  const divServico = botao.closest('.servicos');
+  const paragrafo = divServico.querySelector('.paragrafo');
+
+  if (paragrafo.classList.contains('mostrar')) {
+    paragrafo.classList.remove('mostrar');
+    setTimeout(() => paragrafo.style.display = 'none', 300);
+  } else {
+    paragrafo.style.display = 'flex';
+    setTimeout(() => paragrafo.classList.add('mostrar'), 10);
+  }
+}
